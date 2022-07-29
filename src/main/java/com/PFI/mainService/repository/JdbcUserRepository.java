91d3e22b2edb4a4778d8e184db92fd630a4b1e00
ChangDaeJun <eowns1209@gmail.com>
2022-07-29 17:33:48 +0900

    make JdbcUserRepository and test it.

--- src/main/java/com/PFI/mainService/repository/JdbcUserRepository.java
+++ src/main/java/com/PFI/mainService/repository/JdbcUserRepository.java
@@ -2,13 +2,13 @@ package com.PFI.mainService.repository;
 
 import com.PFI.mainService.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -36,21 +36,35 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public Optional<User> findById(int id) {
-        return Optional.empty();
+        List<User> result = jdbcTemplate.query("select * from USER WHERE ID = ?", userRowMapper() ,id);
+        return result.stream().findAny();
+    }
+
+    private RowMapper<User> userRowMapper() {
+        return ((rs, rowNum) -> {
+           User user = new User();
+           user.setId(rs.getInt("ID"));
+           user.setEmail(rs.getString("EMAIL"));
+           user.setPassword(rs.getString("PASSWORD"));
+           return user;
+        });
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        List<User> result = jdbcTemplate.query("select * from USER WHERE EMAIL = ?", userRowMapper() ,email);
+        return result.stream().findAny();
     }
 
     @Override
     public User update(User user, String newPassword) {
-        return null;
+        jdbcTemplate.update("update USER set PASSWORD = ? WHERE ID = ?", newPassword, user.getId());
+        user.setPassword(newPassword);
+        return user;
     }
 
     @Override
     public void delete(User user) {
-
+        jdbcTemplate.update("delete FROM USER WHERE ID = ?", user.getId());
     }
 }
