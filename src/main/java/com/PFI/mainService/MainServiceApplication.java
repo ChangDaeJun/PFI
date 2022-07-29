db582bc3d4581e7e9a003e13206c91035e111a26
ChangDaeJun <eowns1209@gmail.com>
2022-07-29 10:58:33 +0900

    first commit

---
+++ src/main/java/com/PFI/mainService/MainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.PFI.mainService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PFI/mainService/MainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFI.mainService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
